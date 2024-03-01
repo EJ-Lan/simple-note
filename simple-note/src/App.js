337f062b79f2b5a07153d61c39c47f8635f51897
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-02-29 23:38:53 -0700

    Create Initial components

---
+++ simple-note/src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header';
+import Body from './components/Body'
+import Footer from './components/Footer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
